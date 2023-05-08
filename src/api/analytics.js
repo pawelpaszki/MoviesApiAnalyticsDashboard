c83987e3194b32794792b3cc226aac6d3bca237d
Pawel Paszki <pawelpaszki@gmail.com>
2023-05-08 15:56:03 +0100

    chore: restore previous config

--- src/api/analytics.js
+++ src/api/analytics.js
@@ -1,5 +1,5 @@
 export const getLogsAnalytics = () => {
-  return fetch(`${import.meta.env.VITE_API_URL}/logs/analytics`, {
+  return fetch(`/logs/analytics`, {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${import.meta.env.VITE_SECRET_TOKEN}`
